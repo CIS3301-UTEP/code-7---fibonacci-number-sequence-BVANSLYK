05b1d96bd457a0c46e0675213e92b60dd802c4c9
BVANSLYK <bvanslyke@miners.utep.edu>
2024-10-31 16:50:50 -0600

    Code_7_try_1

--- code_12.py
+++ code_12.py
@@ -1,8 +1,38 @@
 def get_fibonacci_number(position):
-    pass #Remove this line and insert your code here. Do not forget this function implements recursion.
+    if position <= 0:
+        return []
+    elif position == 1:
+        return [0]
+    elif position == 2:
+        return [0, 1]
+    else:
+        sequence = get_fibonacci_number_sequence(position - 1)
+        sequence.append(sequence[-1] + sequence[-2])
+        return sequence[position - 1]
 
 def get_fibonacci_number_sequence(number):
-    pass #Remove this line and insert your code here. Do not forget to use get_fibonacci_number to create your list of numbers.
+    if number <= 0:
+        return []
+    elif number == 1:
+        return [0]
+    elif number == 2:
+        return [0, 1]
+    else:
+        sequence = get_fibonacci_number_sequence(number - 1)
+        sequence.append(sequence[-1] + sequence[-2])
+        return sequence
 
 if __name__ == "__main__":
-    pass #Remove this line and insert your code to test your Fibonacci function here
+    x = int(1)
+    user_position = input("Please enter a positive integer: ")
+    user_position = int(user_position)
+    while(x >= 1):
+        if user_position < 0:
+            user_position = input("This number is not positive, please try another: ")
+            user_position = int(user_position)
+        else:
+            num_at_pos = get_fibonacci_number(user_position)
+            sequence = get_fibonacci_number_sequence(user_position)
+            print(num_at_pos)
+            print(sequence)
+            x = 0
